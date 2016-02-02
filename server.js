b8c154bdfe844373dc19c01bd15083266fd4951b
httpNick <nick.b.duncan@gmail.com>
2016-02-01 23:33:11 -0800

    basic server with socket.io included on both client/server side.

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var socket = require('socket.io')(http);
+
+app.use('/', express.static(__dirname));
+
+app.set('views', __dirname);
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+socket.on('connection', () => {
+
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
